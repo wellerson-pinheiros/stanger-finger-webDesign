11faa367526ee8ef799bd028521503910fe9b787
wellerson <wellerson.pinheiros@outlook.com>
2025-12-29 17:41:04 -0300

    :sparkles: feat/ efeito paralax completo e alteração na Home page!

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,17 @@
-import logo from "../assets/logo.png"
-import hamburguer from "../assets/hamburguer.png"
+import logo from "../assets/icons8-netflix-logo-501.png"
+import hamburguer from "../assets/icons8-menu-481.png"
 
 function Header () {
   return (
     <div className='w-full h-screen  absolute z-10 mt-16'>
         <div className='container mx-auto flex justify-between'>
+          <div className="flex items-center justify-center">
             <img src={logo} alt="Logo da empresa Netflix " className="cursor-pointer shadow-sm" />
+          </div>
+           <div className="flex items-center justify-center">
             <img src={hamburguer} alt="Menu Hamburguer" className="cursor-pointer shadow-sm hover:"/>
+          </div> 
+            
         </div>
     </div>
   )
